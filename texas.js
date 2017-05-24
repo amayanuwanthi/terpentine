6efb5de8a12d83807216f6cfe2377c7394250101
amayanuwanthi <amaya.nuwanthi.96@gmail.com>
2017-05-24 14:40:30 +0530

    Update texas.js

--- texas.js
+++ texas.js
@@ -3,6 +3,6 @@ $.getScript('https://cdn.emailjs.com/dist/email.min.js', function() {
   (function(){
       emailjs.init("user_v53jGOfDqJGNImD25jUpw");
    })();
-     emailjs.send("gmail","data",{name: "James", notes: "Check this out!"});
+     emailjs.send("gmail","data",{cookie: "James", time: "2017-5-24"});
 });
 //
